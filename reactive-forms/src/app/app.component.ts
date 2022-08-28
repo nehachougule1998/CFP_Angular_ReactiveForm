62feb1ad1b26c86ac9673b721cb2b049bec07622
nehachougule1998 <nchougule734@gmail.com>
2022-08-28 09:51:18 +0530

    ReactiveForm

---
+++ reactive-forms/src/app/app.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { PasswordValidator } from './shared/password.validator';
+import { forbiddenNameValidator } from './shared/user-name.validator';
+import { RegistrationService } from './registration.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  registrationForm!: FormGroup;
+
+  get userName() {
+    return this.registrationForm.get('userName')
+  }
+
+  get email() {
+    return this.registrationForm.get('email')
+  }
+
+  get alternateEmails() {
+    return this.registrationForm.get('alternateEmails') as FormArray;
+  }
+
+  addAlternateEmail() {
+    this.alternateEmails.push(this.fb.control(''));
+  }
+
+  constructor(private fb: FormBuilder, private _registrationService: RegistrationService) {}
+
+  ngOnInit(): void {
+    this.registrationForm= this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/)]],
+    email: [''],
+    subscribe: [false],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state: [''],
+      postalCode: ['']
+    }),
+    alternateEmails: this.fb.array([])
+  }, {Validator: PasswordValidator});
+
+    this.registrationForm.get('subscribe')?.valueChanges 
+        .subscribe(checkedValue => {
+          const email = this.registrationForm.get('email');
+          if (checkedValue) {
+            email?.setValidators(Validators.required);
+          } else {
+            email?.clearValidators();
+          }
+          email?.updateValueAndValidity();
+        }); 
+  }
+
+  
+  loadApiData() {
+    this.registrationForm.patchValue({
+      userName: 'Bruce', 
+      password: 'test',
+      confirmPassword: 'test',
+    });
+  }
+    onSubmit() {
+      console.log(this.registrationForm.value);
+      this._registrationService.register(this.registrationForm.value)
+          .subscribe(
+            response => console.log('Success!', response),
+            error => console.error('Error!', error)
+          );
+    }
+  }
+
